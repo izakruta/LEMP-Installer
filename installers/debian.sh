eb0e379b8516b744e2f33ddce3956dcb62cce818
Ilya <izakruta@gmail.com>
2020-03-28 19:00:38 +0300

    Update debian.sh

--- installers/debian.sh
+++ installers/debian.sh
@@ -33,3 +33,11 @@ sudo apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.
 
 #restart MGINX
 sudo systemctl restart nginx
+
+#install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+composer
