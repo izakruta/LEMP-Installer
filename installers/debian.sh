8f72c42230fd07765f6ea6f1b13a6be4f3e3c91a
Ilya <izakruta@gmail.com>
2020-03-28 18:46:39 +0300

    Update debian.sh

--- installers/debian.sh
+++ installers/debian.sh
@@ -26,3 +26,10 @@ sudo apt-get update
 #install Mysql server
 echo -e "Installing PHP-FPM and Mysql extension for PHP..."
 sudo apt-get -y install php7.3-fpm php-mysql
+
+#install PHP extensions
+echo -e "Installing PHP extensions..."
+sudo apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl
+
+#restart MGINX
+sudo systemctl restart nginx
